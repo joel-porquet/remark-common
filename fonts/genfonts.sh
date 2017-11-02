eb4251b6b845b75ff0b7f7d256e8a995b7924710
Joel Porquet <joel@porquet.org>
2017-11-01 22:48:40 -0700

    genfonts.sh: Improve script to handle bold/italic fonts
    Set the weight and style accordingly.
    
    Signed-off-by: Joel Porquet <joel@porquet.org>

--- fonts/genfonts.sh
+++ fonts/genfonts.sh
@@ -1,23 +1,54 @@
 #!/bin/sh
 
-# Safe execution
+## Safe execution
 # -e: exit if subcommand fails
 # -u: unset variable is an error
 # -o pipefail: exit if pipeline fails
 set -eu -o pipefail
 
+## Add TTF file
+function include_ttf {
+
+    # Determine font family
+    ttf_family=$(fc-scan --format="%{family}" "${1}")
+
+    # Determine font weight
+    case "$(fc-scan --format="%{weight}" "${1}")" in
+        "80") ttf_weight="normal";;
+        "200") ttf_weight="bold";;
+        *) echo "Warning: font weight not supported for font ${1}" > 2;
+           return;;
+    esac
+
+    # Determine font slant
+    case "$(fc-scan --format="%{slant}" "${1}")" in
+        "0") ttf_style="normal";;
+        "100") ttf_style="italic";;
+        *) echo "Warning: font slant not supported for font ${1}" > 2;
+           return;;
+    esac
+
+    # Transform TTF content into base64
+    ttf_content_base64=$(base64 -w 0 ${1})
+
+    # Write into CSS
+    addline "@font-face {"
+    addline "\tfont-family: \"${ttf_family}\";"
+    addline "\tfont-weight: \"${ttf_weight}\";"
+    addline "\tfont-style: \"${ttf_style}\";"
+    addline "\tsrc: url(data:font/ttf;base64,${ttf_content_base64}) format('truetype');"
+    addline "}"
+}
+
 function main {
 
-    # Iterate through TTF files
-    for ttf in "${@}";
+    # Iterate through font files
+    for f in "${@}";
     do
-        ttf_name=$(fc-scan --format="%{family}" "${ttf}")
-        ttf_content_base64=$(base64 -w 0 ${ttf})
-        addline "@font-face {"
-        addline "\tfont-family: \"${ttf_name}\";"
-        addline "\tfont-weight: normal;"
-        addline "\tsrc: url(data:font/ttf;base64,${ttf_content_base64}) format('truetype');"
-        addline "}"
+        case "${f##*.}" in
+            "ttf") include_ttf "${f}" ;;
+            *) echo "Warning: ${f} not supported" > 2 ;;
+        esac
     done
 }
 
