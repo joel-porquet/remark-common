ed05a2259d18ee4827f26c18ef3d8f6e344e5e3a
Joel Porquet <joel@porquet.org>
2017-12-06 16:12:08 -0800

    Add new remarkjs macro for newlines
    Signed-off-by: Joel Porquet <joel@porquet.org>

---
+++ remark.newline.js
@@ -0,0 +1,18 @@
+/*
+ * New lines macro
+ *
+ * Syntax: ![:newline num]
+ * - Num can be an integer for multiple lines or a fraction of a line
+ */
+
+remark.macros.newline = function (n) {
+  return '<div style="'
+    + 'height:' + n + 'em; '
+    + 'width:100%; '
+    + 'margin: 0; '
+    + 'padding: 0; '
+    + 'border: 0; '
+    + 'display: block;'
+    + '" ></div>';
+};
+
